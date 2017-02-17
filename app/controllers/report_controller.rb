366869d6f1c9816bc07b815dd91bf55dac64e1d7
JenkaBY <evgen.kuzmich@gmail.com>
2017-02-17 10:59:26 +0300

    Improve report

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -9,23 +9,25 @@ class ReportController < ApplicationController
 
     @start_date   = date_from_hash params[:start_date]
     @end_date     = date_from_hash params[:end_date]
-    @type_id      = params[:type_id].to_i
+    @type_id      = processing_array params[:type_id]
     @category_id  = params[:category_id].to_i
 
     finances = current_user.finances.from_date(@start_date).to_date(@end_date)
 
-    @finances = finances.order(:action_date, :category_id)
-    @income_current_period = @finances.income.total
-    @expense_current_period = @finances.expense.total
+    @finances_for_period  = finances.order(:action_date, :category_id)
+    @total_income_period  = @finances_for_period.income.total
+    @total_expense_period = @finances_for_period.expense.total
 
-    @total = @income_current_period - @expense_current_period
+    @total = @total_income_period - @total_expense_period
 
-    if @type_id == Type::INCOME
-      @categories = finances.income.group(:category_id).select('finances.category_id, sum(finances.amount) as amount')
-      @finances_current = @income_current_period
-    elsif @type_id == Type::EXPENSE
-      @categories = finances.expense.group(:category_id).select('finances.category_id, sum(finances.amount) as amount')
-      @finances_current = @expense_current_period
+    if @type_id.include?(Type::INCOME)
+      @categories_income = finances.income.group(:category_id).select('finances.category_id, sum(finances.amount) as amount')
+      @finances_current_period_income = @total_income_period
+    end
+
+    if @type_id.include?(Type::EXPENSE)
+      @categories_expense = finances.expense.group(:category_id).select('finances.category_id, sum(finances.amount) as amount')
+      @finances_current_period_expense = @total_expense_period
     end
 
 
--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -4,4 +4,12 @@ module ReportHelper
     Date.new *%w(1 2 3).map { |e| hash["date(#{e}i)"].to_i }
   end
 
+  def processing_array array
+    delete_empty_string_from_array array
+    array.map! { |el| el.to_i }
+  end
+
+  def delete_empty_string_from_array array
+    array.delete ''
+  end
 end
